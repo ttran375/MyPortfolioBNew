feb0da5da55f2e4b83d9b76d35a23240d589e30f
ttran375 <ttran375@my.centennialcollege.ca>
2024-06-05 19:33:04 +0000

    chore: Refactor CSS imports for consistency

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 //import { ThemeProvider } from '@material-ui/styles';
-import MainRouter from '../MainRouter';
+import MainRouter from "../MainRouter";
 //import Contact from './contact'
 //import theme from '../theme';
 //import { hot } from 'react-hot-loader'
 const App = () => {
- return (
- <Router>
- 
- <MainRouter />
-
- </Router>
- );
+  return (
+    <Router>
+      <MainRouter />
+    </Router>
+  );
 };
 export default App;
-
