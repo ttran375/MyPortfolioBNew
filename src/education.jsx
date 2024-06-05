651a4161c087bb444ccdf54d1fcb513edb3062f4
ttran375 <ttran375@my.centennialcollege.ca>
2024-06-05 19:33:41 +0000

    Refactor education.jsx for consistency and readability

--- src/education.jsx
+++ src/education.jsx
@@ -1,8 +1,7 @@
 export default function Education() {
-     return (
-     <>
-    <p>Education.Qualification</p>
-     </>
-     );
-     }
-    
+  return (
+    <>
+      <p>Education.Qualification</p>
+    </>
+  );
+}
